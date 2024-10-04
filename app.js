5125ff521139475e12a8438531b2315b22f6dc7e
jamie <level7jamie@gmail.com>
2024-10-04 17:59:03 +0100

    update site content, made an absolute mess of popup content positioning

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 console.log("Connected");
+
+const landscapeButton = document.getElementById("landscape-button");
+const landscapeButtonText = "The funk soul brother...";
+const pTagId = "silly-joke";
+
+landscapeButton.addEventListener("mouseenter", function () {
+  const parent = document.getElementById("hero-1");
+  const newP = document.createElement("p");
+  newP.textContent = landscapeButtonText;
+  newP.id = pTagId;
+  parent.append(newP);
+});
+
+landscapeButton.addEventListener("mouseleave", function () {
+  const tagToRemove = document.getElementById(pTagId);
+  tagToRemove.remove();
+});
+
+const toTop = document.getElementById("back-to-top");
+
+document.addEventListener("scroll", function () {
+  if (
+    document.documentElement.scrollTop > 600 &&
+    toTop.classList.contains("hidden")
+  ) {
+    toTop.classList.remove("hidden");
+  } else if (document.documentElement.scrollTop > 600) {
+    //Do nowt
+  } else {
+    toTop.classList.add("hidden");
+  }
+});
+
+const animalsButton = document.getElementById("btn-animals");
+const animalPopup = document.getElementById("animal-popup");
+const closeAnimalPopup = document.getElementById("animal-close");
+
+animalsButton.addEventListener("click", function () {
+  animalPopup.classList.remove("hidden");
+  console.log("Code ran");
+});
+
+closeAnimalPopup.addEventListener("click", function () {
+  animalPopup.classList.add("hidden");
+  console.log("this ran too!");
+});
